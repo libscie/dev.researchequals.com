e3761e4436c2ea07adec6a45111d124851f03c67
Chris Hartgerink <chris@libscie.org>
2024-03-27 12:07:06 +0100

    Update Hero

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -7,14 +7,8 @@ const HomePage = () => {
       <Metadata title="Home" description="Home page" />
 
       <main>
-        <h1>HomePage</h1>
-        <p>
-          Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-        </p>
-        <p>
-          My default route is named <code>home</code>, link to me with `
-          <Link to={routes.home()}>Home</Link>`
-        </p>
+        <h1>Invest in a future you can believe in</h1>
+        <p>Open access, open source, scholar-led.</p>
       </main>
       <nav id="navbar">
         <ul>
