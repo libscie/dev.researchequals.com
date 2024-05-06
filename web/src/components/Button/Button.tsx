dfc829f7db27f022fb0b50f5f2410196165cd013
Chris Hartgerink <chris@libscie.org>
2024-05-06 20:27:10 +0200

    Factor out Button

---
+++ web/src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+const Button = ({ children }) => {
+  return (
+    <button className="flex h-8 w-auto cursor-pointer items-center rounded-[100px] border-0 bg-violet-600 px-2.5 py-1 text-center font-serif text-xl text-violet-50 hover:bg-violet-500">
+      {children}
+    </button>
+  )
+}
+
+export default Button
--- web/src/components/NavigationBar/NavigationBar.tsx
+++ web/src/components/NavigationBar/NavigationBar.tsx
@@ -1,34 +1,31 @@
-import { Link, routes } from "@redwoodjs/router"
+import { Link, routes } from '@redwoodjs/router'
+import Button from 'src/components/Button'
 
 const NavigationBar = () => {
   return (
-    <header className="fixed bottom-8 w-full m-0 p-0 select-none ">
-      <div className="w-5/6 lg:w-2/3 bg-violet-200 bg-opacity-80 mx-auto rounded-[9999px] h-16 pl-8 pr-4 align-middle backdrop-blur-sm font-serif text-xl text-violet-950">
-       <div className="h-full flex items-center">
-          <nav className="flex-grow flex">
+    <header className="fixed bottom-8 m-0 w-full select-none p-0 ">
+      <div className="mx-auto h-16 w-5/6 rounded-[9999px] bg-violet-200 bg-opacity-80 pl-8 pr-4 align-middle font-serif text-xl text-violet-950 backdrop-blur-sm lg:w-2/3">
+        <div className="flex h-full items-center">
+          <nav className="flex flex-grow">
             <div className="hidden sm:inline [&>a]:text-violet-950 [&>a]:no-underline hover:[&>a]:text-violet-700">
-              <Link to={routes.home()}  className="mr-2">
+              <Link to={routes.home()} className="mr-2">
                 Home
               </Link>
             </div>
-            <div className="flex-grow mx-auto text-left sm:text-center [&>a]:text-violet-950 [&>a]:no-underline hover:[&>a]:text-violet-700">
-              <Link to={routes.home()}  className="mx-2">
+            <div className="mx-auto flex-grow text-left sm:text-center [&>a]:text-violet-950 [&>a]:no-underline hover:[&>a]:text-violet-700">
+              <Link to={routes.home()} className="mx-2">
                 Read
               </Link>
-              <Link to={routes.home()}  className="mx-2">
+              <Link to={routes.home()} className="mx-2">
                 Publish
               </Link>
-              <Link to={routes.home()}  className="mx-2">
+              <Link to={routes.home()} className="mx-2">
                 Curate
               </Link>
             </div>
           </nav>
-          <div
-            className="rounded-[100px] bg-violet-600 px-2.5 py-1 h-8 w-20 text-center text-violet-50 hover:bg-violet-500 cursor-pointer"
-          >
-            Sign up
-          </div>
-      </div>
+          <Button>Sign up</Button>
+        </div>
       </div>
     </header>
   )
