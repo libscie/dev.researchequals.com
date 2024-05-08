434bf1e0d345a3a7fce91ae4227b841d5c3a1be0
Chris Hartgerink <chris@libscie.org>
2024-05-08 10:32:53 +0200

    Improve responsiveness

--- web/src/components/MetadataContainer/MetadataContainer.tsx
+++ web/src/components/MetadataContainer/MetadataContainer.tsx
@@ -1,7 +1,7 @@
 const MetadataContainer = ({ children }) => {
   return (
     <div className="flex text-right lg:block lg:w-2/5 lg:p-2 lg:px-4">
-      <span className="sticky top-2">
+      <span className="sticky top-2 mx-auto">
         {children}
         <hr />
       </span>
