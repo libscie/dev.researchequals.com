7428a411a2ae528a8c8e84e8ce37b9dea4556f83
Chris Hartgerink <chris@libscie.org>
2024-06-07 14:19:16 +0200

    Remove placeholder text

--- web/src/pages/SettingsWorkspacePage/SettingsWorkspacePage.tsx
+++ web/src/pages/SettingsWorkspacePage/SettingsWorkspacePage.tsx
@@ -12,17 +12,7 @@ const SettingsWorkspacePage = () => {
           <SettingsNav current="Workspace" />
         </MetadataContainer>
         <div className="w-full p-0">
-          <div className="max-w-2xl text-left">
-            My research focuses on how the publishing system can facilitate
-            improved quality of scholarly research. In particular, my research
-            explores modular publishing systems that break a research project
-            into its building blocks. These building blocks are then sharing
-            continuously and in chronological order, to provide a more accurate
-            depiction of the research journey. I am currently director of the
-            organization Liberate Science GmbH to implement this research
-            finding into practice, and develop the practical use of it within a
-            diverse set of research communities.
-          </div>
+          <div className="max-w-2xl text-left">TBD</div>
         </div>
       </main>
     </>
