d95008ed24fb6554e523f68aa329b6fc9d50044e
Chris Hartgerink <chris@libscie.org>
2024-06-08 17:47:11 +0200

    Add nav to admin + mod

--- web/src/pages/AdministrationPage/AdministrationPage.tsx
+++ web/src/pages/AdministrationPage/AdministrationPage.tsx
@@ -1,20 +1,57 @@
-import { Link, routes } from '@redwoodjs/router'
+import {
+  AirlinePassengerCare,
+  Bullhorn,
+  CloudAuditing,
+  DocumentMultiple_01,
+  Finance,
+  RuleFilled,
+  User,
+} from '@carbon/icons-react'
+
+import { routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+
+import MetadataContainer from 'src/components/MetadataContainer/MetadataContainer'
+import SecondaryNav from 'src/components/SecondaryNav/SecondaryNav'
 
 const AdministrationPage = () => {
   return (
     <>
       <Metadata title="Administration" description="Administration page" />
-
-      <h1>AdministrationPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/AdministrationPage/AdministrationPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>administration</code>, link to me with `
-        <Link to={routes.administration()}>Administration</Link>`
-      </p>
+      <main className="h-full w-full lg:flex">
+        <MetadataContainer>
+          <SecondaryNav
+            items={[
+              {
+                name: 'Announcement',
+                href: routes.administration(),
+                icon: Bullhorn,
+              },
+              {
+                name: 'Testimonials',
+                href: routes.administration(),
+                // eslint-disable-next-line react/jsx-no-undef
+                icon: AirlinePassengerCare,
+              },
+              {
+                name: 'Log',
+                href: routes.administration(),
+                icon: CloudAuditing,
+              },
+              {
+                name: 'Server rules',
+                href: routes.administration(),
+                icon: RuleFilled,
+              },
+            ]}
+            header="Administration"
+            current="General"
+          />
+        </MetadataContainer>
+        <div className="w-full p-0">
+          <div className="max-w-2xl text-left">TBD</div>
+        </div>
+      </main>
     </>
   )
 }
--- web/src/pages/ModerationPage/ModerationPage.tsx
+++ web/src/pages/ModerationPage/ModerationPage.tsx
@@ -1,20 +1,43 @@
-import { Link, routes } from '@redwoodjs/router'
+import { DocumentMultiple_01, Finance, User } from '@carbon/icons-react'
+
+import { routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+
+import MetadataContainer from 'src/components/MetadataContainer/MetadataContainer'
+import SecondaryNav from 'src/components/SecondaryNav/SecondaryNav'
 
 const ModerationPage = () => {
   return (
     <>
       <Metadata title="Moderation" description="Moderation page" />
-
-      <h1>ModerationPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/ModerationPage/ModerationPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>moderation</code>, link to me with `
-        <Link to={routes.moderation()}>Moderation</Link>`
-      </p>
+      <main className="h-full w-full lg:flex">
+        <MetadataContainer>
+          <SecondaryNav
+            items={[
+              {
+                name: 'Queue',
+                href: routes.moderation(),
+                icon: DocumentMultiple_01,
+              },
+              {
+                name: 'First contributors',
+                href: routes.moderation(),
+                icon: User,
+              },
+              {
+                name: 'Reports',
+                href: routes.moderation(),
+                icon: Finance,
+              },
+            ]}
+            header="Moderation"
+            current="General"
+          />
+        </MetadataContainer>
+        <div className="w-full p-0">
+          <div className="max-w-2xl text-left">TBD</div>
+        </div>
+      </main>
     </>
   )
 }
