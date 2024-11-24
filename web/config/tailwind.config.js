4a3db4fde74e0f2defd01c011c95535c5f4c9622
Chris Hartgerink <chris@libscie.org>
2024-11-24 12:19:54 +0100

    Add font styling

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      // Font stacks from: https://modernfontstacks.com/
+      fontFamily: {
+        serif: 'Iowan Old Style, Palatino Linotype, URW Palladio L, P052',
+        sans: 'Inter, Roboto, Helvetica Neue, Arial Nova, Nimbus Sans, Arial, sans-serif',
+      },
+    },
   },
   plugins: [],
 }
-
