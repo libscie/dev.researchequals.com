b89df5c89b392410bee1faa3778f08c6316019a6
Chris Hartgerink <chris@libscie.org>
2025-01-06 17:18:11 +0100

    Add `uninvert()` utility
    OpenAlex uses inverted indexes to represent the
    abstract as data. See also
    https://docs.openalex.org/api-entities/works/work-object#abstract_inverted_index
    This utility helps convert the inverted index into
    a usable form.

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -1,3 +1,12 @@
 export function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
+
+export function uninvert(obj) {
+  const arr = []
+  // If to robustify function
+  if (obj) {
+    Object.keys(obj).forEach((k) => obj[k].forEach((a) => (arr[a] = k)))
+    return arr.join(' ')
+  }
+}
