c403e82fa05516e350f6584b83764f5502f22dfb
Chris Hartgerink <chris@libscie.org>
2025-01-07 09:32:31 +0100

    Add `fetchOpenAlexWorks` service

--- web/src/services/openAlex.ts
+++ web/src/services/openAlex.ts
@@ -7,6 +7,15 @@ export const fetchOpenAlexWork = async (id: string) => {
   return data
 }
 
+export const fetchOpenAlexWorks = async (url: string) => {
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw new Error('Failed to fetch data from OpenAlex API')
+  }
+  const data = await response.json()
+  return data.results
+}
+
 export const fetchOpenAlexAuthor = async (id: string) => {
   const response = await fetch(`https://api.openalex.org/people/A${id}`)
   if (!response.ok) {
