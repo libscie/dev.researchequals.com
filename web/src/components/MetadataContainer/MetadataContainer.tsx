aea66bde28381a821b13a964bd764c957b49116c
Chris Hartgerink <chris@libscie.org>
2025-01-11 10:42:08 +0100

    Improve rendering of `MetadataContainer`

--- web/src/components/MetadataContainer/MetadataContainer.tsx
+++ web/src/components/MetadataContainer/MetadataContainer.tsx
@@ -1,9 +1,9 @@
 const MetadataContainer = ({ children }) => {
   return (
     <div className="flex text-left lg:block lg:w-2/5 lg:p-2 lg:px-4">
-      <span className="sticky top-2 mx-auto block">
+      <span className="top-2 block w-full px-4 md:sticky">
         {children}
-        <hr />
+        {/* <hr /> */}
       </span>
     </div>
   )
