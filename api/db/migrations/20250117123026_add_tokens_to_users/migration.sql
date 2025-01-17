15823e0873ce4654653bc9c3407e3a8f5bd6f80a
Chris Hartgerink <chris@libscie.org>
2025-01-17 14:57:32 +0100

    Add necessary models for 2FA process

---
+++ api/db/migrations/20250117123026_add_tokens_to_users/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `Token` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `hashedToken` VARCHAR(191) NOT NULL,
+    `type` ENUM('OTP') NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Token_hashedToken_type_key`(`hashedToken`, `type`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Token` ADD CONSTRAINT `Token_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
